224a010d6f2fc66e7e69b26860b824ffc8cd37a3
manfred-commits <manfredipiraino@gmail.com>
2021-08-31 14:02:16 +0200

    aggiunto html e js di base

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,12 +1,23 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
+
 // Consigli del giorno
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+// Una parola si definisce palindroma, se leggendola dall'inizio alla fine e viceversa, viene letta nello stesso modo.
+
+// es: anna
+
+// 1. creare un evento che fornisca, al click del bottone Invio, se la frase inserita dall'utente (nell'input) è palindroma
+
+var btnEnter = document.getElementById("enter");
+
+btnEnter.addEventListener("click",
+function(){
+    alert("hello!");
+    var userWord = document.getElementById("word").value;
+    console.log(userWord);
+}
+)
