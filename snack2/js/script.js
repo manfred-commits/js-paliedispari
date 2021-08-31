ed14a3392c292d0a5b3684b3d64afca07582206e
manfred-commits <manfredipiraino@gmail.com>
2021-08-31 14:57:19 +0200

    aggiunto html e js di base

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,12 +1,22 @@
-// Palidroma
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
 // Consigli del giorno
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+// 1.Creare un evento in cui vengono raccolti i dati inseriti dall'utente:
+// dati sul numero inserito;
+// e se il numero che il computer genera sarà pari o dispari.
+
+var btnEnter = document.getElementById("enter");
+
+btnEnter.addEventListener("click",
+function(){
+    
+}
+)
